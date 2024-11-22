8f679485a547b18c5f32629ee75d1473a0c030fb
Philipp Koch <PhillKoch@protonmail.com>
2024-11-22 22:54:44 +0100

    Refactor, update .gitignore

--- promptzl/classes.py
+++ promptzl/classes.py
@@ -315,7 +315,11 @@ class LLM4ClassificationBase(torch.nn.Module):
                 return logits, outputs
             else:
                 return logits
-        probs: tensor = torch.nn.functional.softmax(logits, dim=1) if temperature == 1.0 else torch.nn.functional.softmax(logits / temperature, dim=1)
+        probs: tensor = (
+            torch.nn.functional.softmax(logits, dim=1)
+            if temperature == 1.0
+            else torch.nn.functional.softmax(logits / temperature, dim=1)
+        )
         if calibrate:
             probs = self._calibrate(probs)
         if return_model_output:
@@ -489,9 +493,10 @@ class LLM4ClassificationBase(torch.nn.Module):
                 )
                 return_dict[key] = results
             return return_dict
-        else:   
+        else:
             # TODO: test this case
             raise ValueError("Data must be of type Dataset or DatasetDict.")
+
 
 class MaskedLM4Classification(LLM4ClassificationBase, torch.nn.Module):
     """Masked-Language-Modeling-Based Classification.
