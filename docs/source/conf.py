75176d8b9b2171addfe6a401993956ae42d7da5d
Philipp Koch <PhillKoch@protonmail.com>
2024-11-29 17:30:38 +0100

    Update conf.py for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 
 # sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath('../../promptzl'))
+sys.path.insert(0, os.path.abspath('../../..'))
 
 project = 'Promptzl'
 copyright = '2024, Philipp Koch'
