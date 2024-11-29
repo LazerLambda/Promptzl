2b2ef763ccbe6df58f38eecb8e26ef299f585637
Philipp Koch <PhillKoch@protonmail.com>
2024-11-29 17:40:20 +0100

    Update conf.py for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,7 @@ import os
 import sys
 
 # sys.path.insert(0, os.path.abspath("."))
+print(os.listdir(os.path.abspath(".")))
 print(os.path.abspath('../../../..'))
 print(os.listdir(os.path.abspath('../../../..')))
 sys.path.insert(0, os.path.abspath('../../..'))
