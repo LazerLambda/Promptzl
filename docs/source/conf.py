77a314154d5d0a06cbc111c4d0703535528bf5c1
Philipp Koch <PhillKoch@protonmail.com>
2024-11-29 17:14:33 +0100

    Update conf.py for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+
 project = 'Promptzl'
 copyright = '2024, Philipp Koch'
 author = 'Philipp Koch'
